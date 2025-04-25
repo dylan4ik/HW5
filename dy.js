d2b7a73506303ac41b1f13a85b261fca90c7076a
dylan4ik <129986777+dylan4ik@users.noreply.github.com>
2025-04-25 17:22:02 +0200

    menu

---
+++ dy.js
@@ -0,0 +1,15 @@
+let choice = "tea";
+
+switch (choice) {
+  case "coffee":
+    console.log("выбор:coffee");
+    break;
+  case "Чай":
+    console.log("выбор:tea");
+    break;
+  case "juice":
+    console.log("выбор:juise");
+    break;
+  default:
+    console.log("этого нету в списке");
+}
